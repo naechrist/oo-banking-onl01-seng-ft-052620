0fa69ccddd7cf2f53df9456fe6eb8f2057936d29
Jenae Christensen <729jenae.christensen@gmail.com>
2020-06-20 17:29:10 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -13,13 +13,12 @@ class Transfer
   end
   
   def execute_transaction
-    if @sender.balance > @amount && @status == "pending"
+    if @sender.valid? && @sender.balance > self.amount && @status == "pending"
       @sender.balance -= @amount
       @receiver.balance += @amount
       @status = "complete"
     else
-      
-      @status = "closed"
+      @status = "rejected"
       return "Transaction rejected. Please check your account balance."
     end
   end
