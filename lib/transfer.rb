e67bf38931672a81108944e54032c83fcfde426f
Jenae Christensen <729jenae.christensen@gmail.com>
2020-06-20 16:37:40 +0000

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -19,7 +19,7 @@ class Transfer
       @status = "complete"
     else
       
-      @status = "rejected"
+      @status = "closed"
       return "Transaction rejected. Please check your account balance."
     end
   end
